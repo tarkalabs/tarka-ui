5c97d6bfa60d1b58565cf0190e44712c7dd442ff
Mitchell MacDonald <mitchellmacdonald@Sedins-MacBook-Pro-2.local>
2022-08-15 13:22:28 -0300

    worked on the floating action button and json tokens

--- src/stories/FloatingActionButton.stories.tsx
+++ src/stories/FloatingActionButton.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import '../assets/images/Shape.png';
 import { FloatingActionButton } from '../components/FloatingActionButton';
 
 export default {
@@ -8,7 +8,18 @@ export default {
   component: FloatingActionButton,
 } as ComponentMeta<typeof FloatingActionButton>;
 
-const Template: ComponentStory<typeof FloatingActionButton> = (args: any) => <FloatingActionButton {...args} />;
+const Template: ComponentStory<typeof FloatingActionButton> = (args) => <FloatingActionButton {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {};
+
+export const NoIcon = Template.bind({});
+
+NoIcon.args = {
+  size:'regular',
+};
+
+export const icon = Template.bind({});
+
+icon.args = {
+  size:'large',
+  icon: "https://www.w3schools.com/images/lamp.jpg"
+};
---
+++ src/utils/ThemeParse.tsx
@@ -0,0 +1,60 @@
+import { start } from "repl";
+import lightMode from "../data/lightMode.json"
+
+
+function getValueAtKeys(keys:string[]){
+    let currentValue:any = lightMode;
+    for(let key of keys){       
+        currentValue = currentValue[key as keyof typeof currentValue];
+    }
+    let jsonValue =  currentValue.value;
+    return jsonValue;
+}
+
+
+export function getBracketValue(stringIn:string){
+    let keys= stringIn.slice(1, -1).split('.');
+    return getValueAtKeys(keys);
+}
+
+
+export function curlyScanner(stringIn:string):string{
+    let bracket = [];
+    let found = false;
+
+    for(let i = 0; i < stringIn.length; i++){
+        if(stringIn[i] === '{'){
+            found = true;
+        }
+        else if(stringIn[i] === '}'){
+            bracket.push(stringIn.charAt(i));
+            break;
+        }
+
+        if(found){
+            bracket.push(stringIn.charAt(i));
+        }
+    }
+
+    if(found){
+        let newString = stringIn.replace(bracket.join(''),getBracketValue(bracket.join('')));
+        return curlyScanner(newString);
+    }
+
+    return stringIn;
+}
+
+
+
+export function getJSONValue(stringIn:string){
+    let keys = stringIn.split('/');
+    let jsonValue = getValueAtKeys(keys);
+    return curlyScanner(jsonValue);
+}
+
+export function setUpTokens(tokenNames:string[]){
+    let float:any = document.querySelector(':root');
+    for(let token of tokenNames){
+        float.style.setProperty(`--${token.replaceAll('/','-')}`,getJSONValue(token));
+    }
+}
