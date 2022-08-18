747aa7c254ed3c325310dd000dabf5dae06e2c5d
Kayla <khawkin2@unb.ca>
2022-08-18 09:51:39 -0300

    change icon to use ReactNode

--- src/components/SuccessCheck/SuccessCheck.tsx
+++ src/components/SuccessCheck/SuccessCheck.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import './SuccessCheck.css'
 import '../../utils/ThemeParse'
 import { setUpTokens } from "../../utils/ThemeParse";
-import { ReactComponent as CheckmarkIcon } from '@icons/checkmark.svg';
+import CheckmarkIcon from '@icons/checkmark.svg';
 
-const SuccessCheck: React.FC= function ({}) {
+const SuccessCheck: React.FC= function () {
     setUpTokens(['success/success', 'success/on-success']);
 
     return (
         <div className="SuccessCheck">
             <div className="success">
-                <CheckmarkIcon/>
+                <img src={CheckmarkIcon} alt="logo"/>
             </div>
         </div>
     )
