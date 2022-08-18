b8a08b488493cd589bca4443a224122e380b3acb
renglan1 <rob.eng@unb.ca>
2022-08-18 14:59:10 -0300

    removed zeromd dep

---
+++ src/stories/views/DocPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './DocPage.css';
+import { setUpTokens } from '../../utils/ThemeParse'
+import { ReactComponent as PlusIcon } from '@icons/plus.svg';
+
+interface props {
+  name: string,
+  mdx: string
+}
+
+const DocPage: React.FC<props> = ({ name, mdx }) => {
+  return (
+    <div className={`doc-page ${name}`}>
+
+    </div>
+  );
+};
+
+export default DocPage;
