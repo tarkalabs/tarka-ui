f2a2437dedad3a302b3aff7c620b9cefc6088b3d
renglan1 <rob.eng@unb.ca>
2022-08-18 15:23:00 -0300

    Fixed issue with main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,8 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-    '@storybook/addon-links'
+    "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
   "core": {
