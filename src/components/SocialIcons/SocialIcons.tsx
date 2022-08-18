d627e0a0dcb2085332f7e88d8bb6db813bb3a94e
Kayla <khawkin2@unb.ca>
2022-08-18 12:23:31 -0300

    update format

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -4,32 +4,32 @@ import '../../utils/ThemeParse'
 import { setUpTokens } from "../../utils/ThemeParse";
 
 interface props {
-  children: ReactNode,
-  classes: object,
-  icons: (
-        'applemusic'| 
-        'clubhouse' | 
-        'facebook'  | 
+    children: ReactNode,
+    classes: object,
+    icons: (
+        'applemusic' |
+        'clubhouse' |
+        'facebook' |
         'instagram' |
-        'linkedin'  | 
-        'soundcloud'| 
-        'spotify'   | 
-        'tiktok'    |
-        'twitch'    | 
-        'twitter'   | 
+        'linkedin' |
+        'soundcloud' |
+        'spotify' |
+        'tiktok' |
+        'twitch' |
+        'twitter' |
         'youtube'
     )[]
 }
-const SocialIcons: React.FC<props> = function ({ classes, icons}) {
-  setUpTokens(['surface/on-surface']);
+const SocialIcons: React.FC<props> = function ({ classes, icons }) {
+    setUpTokens(['surface/on-surface']);
 
-  return (
-    <div className={`social-icons ${classes}`}>
-        <div className="socials">
-            {icons.map(icon => <img src={require(`@icons/social/${icon}.svg`)} alt=""></img>)}
+    return (
+        <div className={`social-icons ${classes}`}>
+            <div className="socials">
+                {icons.map(icon => <img src={require(`@icons/social/${icon}.svg`)} alt=""></img>)}
+            </div>
         </div>
-    </div>
-  )
+    )
 
 }
 
