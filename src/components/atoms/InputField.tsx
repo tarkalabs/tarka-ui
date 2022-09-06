c6968e061ee1e298d0cf86ccf125ccecea8d37c1
renglan1 <rob.eng@unb.ca>
2022-09-06 08:46:58 -0300

    added hover states

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -1,44 +1,57 @@
-import React from 'react';
-import InputUnstyled from '@mui/base/InputUnstyled';
-import {InputUnstyledProps} from '@mui/base/InputUnstyled';
-import { styled } from '@mui/system';
-import { ReactNode } from 'react';
-import { setUpTokens } from '@/utils/ThemeParse';
-import { bodySize5 } from '@/assets/fonts/fonts';
+import React from "react";
+import InputUnstyled from "@mui/base/InputUnstyled";
+import { InputUnstyledProps } from "@mui/base/InputUnstyled";
+import { styled } from "@mui/system";
+import { ReactNode } from "react";
+import { setUpTokens } from "@/utils/ThemeParse";
+import { bodySize5 } from "@/assets/fonts/fonts";
 
-
-interface FrameProps {
-
-}
+interface FrameProps {}
 
 export type InputFieldProps = FrameProps & InputUnstyledProps;
 
 const InputFieldFrame = styled(InputUnstyled)`
     input {
         background-color: var(--input-input-background);
+        border-width: 0;
         border-radius: 8px;
-        border-width: 0 0 2px 0;
         min-height: 20px;
         min-width: 175px;
         color: var(--input-text-dim);
         padding: 17px 16px;
+        outline: none;
         ${bodySize5}
 
-        &:focus {
-            outline: none;
+        &:hover {
+            border-color: var(--utility-disabled-content);
+            border-width: 0 0 2px 0;
+
+            &:focus {
+                border-color: var(--primary-primary);
+                border-width: 0 0 2px 0;
+            }
         }
     }
-`
+`;
 
 const InputField: React.FC<InputFieldProps> = function ({ ...props }) {
-    setUpTokens(['input/input-background', 'input/text-dim', 'primary/primary', 'error/error', 'warning/warning', 'success/success',
-        'utility/disabled-background', 'utility/disabled-content']);
+    setUpTokens([
+        "input/input-background",
+        "input/text-dim",
+        "primary/primary",
+        "error/error",
+        "warning/warning",
+        "success/success",
+        "utility/disabled-background",
+        "utility/disabled-content",
+    ]);
 
     return (
-        <InputFieldFrame className={props.className} {...props} >
-
-        </InputFieldFrame>
-    )
-}
+        <InputFieldFrame
+            className={props.className}
+            {...props}
+        ></InputFieldFrame>
+    );
+};
 
 export default InputField;
