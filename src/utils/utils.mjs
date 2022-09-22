ed763993d03b9e9835d6fc63d3970c859372578c
renglan1 <rob.eng@unb.ca>
2022-09-22 11:03:50 -0300

    removed unused file

--- src/utils/utils.mjs
+++
@@ -1,7 +0,0 @@
-import React, { createElement } from "react";
-import { renderToStaticMarkup } from "react-dom/server";
-
-export const reactSvgComponentToMarkupString = (component, props) =>
-`data:image/svg+xml,${encodeURIComponent(
-  renderToStaticMarkup(createElement(component, props))
-)}`;
